cb4923d4fcbfd232f115cd7d4567aac587993946
unknown <ye37seol@gmailcom>
2024-03-28 15:07:31 -0700

    first commit

---
+++ databaseConnection.js
@@ -0,0 +1,10 @@
+const MongoClient = require("mongodb").MongoClient;
+const is_qoddi = process.env.IS_QODDI || false;
+const qoddiURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
+if (is_qoddi) {
+  var database = new MongoClient(qoddiURI, { useNewUrlParser: true, useUnifiedTopology: true });
+} else {
+  var database = new MongoClient(localURI, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+module.exports = database;
