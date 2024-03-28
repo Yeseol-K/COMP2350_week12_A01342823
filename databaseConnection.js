fdcdb5bad77eb2aca78e8b50025001c0e59de66b
unknown <ye37seol@gmailcom>
2024-03-28 15:10:56 -0700

    fix mongo db

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const is_qoddi = process.env.IS_QODDI || false;
-const qoddiURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const qoddiURI = "mongodb+srv://johnsmith890328:gftScSTsQzFLuw5L@cluster0.zjxitka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
 if (is_qoddi) {
   var database = new MongoClient(qoddiURI, { useNewUrlParser: true, useUnifiedTopology: true });
